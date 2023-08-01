0fb5f16a987ecba551f6fdedca4cbd32ae3651cb
LeandroSouzaOliveira <leandro.oliveirasouza1@gmail.com>
2023-08-01 08:39:02 -0300

    Access control

--- src/main/java/com/devsuperior/dscommerce/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/dscommerce/config/ResourceServerConfig.java
@@ -3,6 +3,7 @@ package com.devsuperior.dscommerce.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
@@ -13,6 +14,7 @@ import java.util.Arrays;
 
 @Configuration
 @EnableResourceServer
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Autowired
@@ -21,7 +23,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
     @Autowired
     private JwtTokenStore tokenStore;
 
-    private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
+    //private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" }; -> optou-se por ajuste fino no controller e liberar todos
 
     @Override
     public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
@@ -30,10 +32,10 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        // H2 frames
+        //H2
         if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
             http.headers().frameOptions().disable();
         }
-        http.authorizeRequests().antMatchers(PUBLIC).permitAll().anyRequest().authenticated();
+        http.authorizeRequests().anyRequest().permitAll(); //.antMatchers(PUBLIC).permitAll().anyRequest().authenticated();
     }
 }
